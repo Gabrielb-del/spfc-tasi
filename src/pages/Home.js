296e790d54baf42d3e3af5d8deeb71b9d8302f2d
Gabrielb-del <gabrielbaunilia123@gmail.com>
2025-05-15 23:03:05 -0300

    15.05

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Container } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 import CarouselBanner from '../components/CarouselBanner';
 
 const Home = () => {
     return (
         <Container disableGutters maxWidth={false}>
             <CarouselBanner />
+            <Typography>Teste</Typography>
         </Container>
     );
 };
